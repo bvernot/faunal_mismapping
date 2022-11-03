ee5a1dae2ad20d521eef7af100ab2049abd0c73e
Robin_Warner <robwar94@gmail.com>
2022-11-03 10:18:15 +0100

    restarted the mapping with 30m seqs in the evening but forgot to commit

--- analysis/first_simulations/mapping.sh
+++ analysis/first_simulations/mapping.sh
@@ -4,19 +4,22 @@ basedirectory="/mnt/expressions/benjamin_vernot/faunal_mismapping/data/simulated
 sizedistribution=$basedirectory"/size_distribution.tsv"
 genomefilename="/mnt/expressions/robin_warner/robin_genomes/human/human_genome.fna"
 
+echo "mapping"
 for directoryname in "$basedirectory"/*; do 
     if [ -d "$directoryname" ]; then 
+        echo $directoryname
         species=${directoryname##*_}
         queryfilename=$directoryname"/"$species"_d.fa.gz"
         outfile=$directoryname"/"$species"_to_human.sai"
         logfile=$directoryname"/mapping.log"
 
-        bwa aln $genomefilename $queryfilename 1> $outfile 2> $logfile &
+        bwa aln $genomefilename -n 0.01 -o 2 -l 16500 $queryfilename 1> $outfile 2> $logfile &
     fi
 done 
 
 wait
 
+echo "converting to bam"
 for directoryname in "$basedirectory"/*; do 
     if [ -d "$directoryname" ]; then 
         species=${directoryname##*_}
@@ -26,6 +29,6 @@ for directoryname in "$basedirectory"/*; do
         bamoutfile=$directoryname"/"$species"_to_human.bam"
 
         bwa samse $genomefilename $outfile $queryfilename 2> $logfile | samtools view -Sb > $bamoutfile &
+        
     fi
 done 
-
--- analysis/first_simulations/simulations.sh
+++ analysis/first_simulations/simulations.sh
@@ -4,14 +4,16 @@ basedirectory="/mnt/expressions/benjamin_vernot/faunal_mismapping/data/simulated
 sizedistribution=$basedirectory"/size_distribution.tsv"
 
 for directoryname in "$basedirectory"/*; do 
-    if [ -d "$directoryname" ]; then 
+    if [ -d "$directoryname" ]; then
+        echo $directoryname
         species=${directoryname##*_}
         fastafilename=$directoryname"/endo/"$species"_genome.fna"
         results=$directoryname/$species
         stdout=$directoryname"/stdout.log"
         stderr=$directoryname"/stderr.log"
 
-        gargammel -f $sizedistribution -n 10000 -o $results -damage 0.03,0.4,0.01,0.3 $directoryname 1> $stdout 2> $stderr &
+        gargammel -f $sizedistribution -n 30000000 -o $results -damage 0.03,0.4,0.01,0.3 $directoryname 1> $stdout 2> $stderr &
     fi
 done
-
+wait
+echo "done"
