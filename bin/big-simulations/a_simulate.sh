9c327a0866fcb9f4c43dd1d926e22e1cff72b455
Robin_Warner <robwar94@gmail.com>
2022-11-24 15:53:24 +0100

    first steps of faunalization pipeline, currently mapping to human ref

--- bin/big-simulations/a_simulate.sh
+++ bin/big-simulations/a_simulate.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 resultsDir="/mnt/expressions/benjamin_vernot/faunal_mismapping/data/big-simulations/1_simulations"
 sizeDistribution="/mnt/expressions/benjamin_vernot/faunal_mismapping/data/once-again/0_genomes/size_distributions.tsv"
 
@@ -8,12 +7,22 @@ dogGenomeDir="/mnt/expressions/benjamin_vernot/faunal_mismapping/data/once-again
 bearGenomeDir="/mnt/expressions/benjamin_vernot/faunal_mismapping/data/once-again/0_genomes/3_bear"
 sheepGenomeDir="/mnt/expressions/benjamin_vernot/faunal_mismapping/data/big-simulations/0_genomes/0_sheep"
 
-# gargammel usage
-echo gargammel -f $sizeDistribution -n 10000000 -o $outputDir -damage 0.03,0.4,0.01,0.3 $speciesDir 1> $stdout 2> $stderr
-
 for genomeDir in $dogGenomeDir $bearGenomeDir $sheepGenomeDir; do
     species=`echo $genomeDir | awk -F "_" '{print $NF}'`
-    echo $genomeDir $species
+    echo $species
 
+    speciesDir=$resultsDir"/"$species
+    mkdir -p $speciesDir
 
+    for i in {1..20}; do
+        finalDir=$speciesDir"/"$i
+        mkdir -p $finalDir
+
+        stdout=$finalDir"/stdout.log"
+        stderr=$finalDir"/stderr.log"
+
+        gargammel -f $sizeDistribution -n 30000000 -o $finalDir"/"species -damage 0.03,0.4,0.01,0.3 $genomeDir 1> $stdout 2> $stderr &
+
+    done
+    wait
 done
